e2c3d66631ac526bebd78b9530141c55028368f5
JaFaR <sysadjafarpr@gmail.com>
2020-01-04 10:15:48 +0300

    initial version

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+//ctrl + alt + v выделением- позволит создать локалбную перемену
+
+//shift + f6 переменование имени
+func main() {
+	scores :=[3]int{10,7,10}
+	promoters :=0
+
+
+
+
+
+}
